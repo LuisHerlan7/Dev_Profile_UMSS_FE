a5f8ea3ebf6aebd1381eaf3647fd37398b10c679
Nioxi1 <201900804@est.umss.edu>
2026-03-20 23:35:15 -0400

    ya esta en parte la vista de visitante, falta pulir aun algunas cosas

--- src/app/AppRouter.tsx
+++ src/app/AppRouter.tsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { HomePage } from '../pages/HomePage';
+import { VisitanteOfertaPortafolioPage } from '../pages/VisitanteOfertaPortafolioPage';
+import { VisitantePortafolioPage } from '../pages/VisitantePortafolioPage';
 import { LoginPage } from '../features/auth/pages/LoginPage';
 import { DashboardPage } from '../features/dashboard/pages/DashboardPage';
 
@@ -22,6 +24,8 @@ export function AppRouter() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/visitante" element={<VisitanteOfertaPortafolioPage />} />
+        <Route path="/portafolio/:id" element={<VisitantePortafolioPage />} />
         <Route path="/admin" element={<AdminPage />} />
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/explore" element={<ExplorePage />} />
